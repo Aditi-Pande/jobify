fbdb8170958b8a37dff1fe0f7d57f34ad1353013
Aditi Pande <apande1802@gmail.com>
2024-10-19 10:24:16 +0530

    import error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useEffect } from 'react';
 
 import {
   HomeLayout,
